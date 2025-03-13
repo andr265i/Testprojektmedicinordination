e70dd0739d9d227fadcf5910dfcb998986c056b1
André Eickmayer Hansen <andr265i@gmail.com>
2025-03-13 11:40:03 +0100

    Sammenhæng og TODO lavet på DagligFast og PN

--- Medicinordination/src/ordination/DagligFast.java
+++ Medicinordination/src/ordination/DagligFast.java
@@ -1,5 +1,58 @@
 package ordination;
 
-public class DagligFast {
-    // TODO
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+
+public class DagligFast extends Ordination {
+
+    private final Dosis[] doser = new Dosis[4];
+
+    public DagligFast(LocalDate startDen, LocalDate slutDen, Patient patient,
+                      Laegemiddel laegemiddel, double morgenAntal,
+                      double middagAntal, double aftenAntal, double natAntal) {
+
+        super(startDen, slutDen, patient, laegemiddel);
+        doser[0] = createDosis(LocalTime.of(6, 0), morgenAntal);
+        doser[1] = createDosis(LocalTime.of(12, 0), middagAntal);
+        doser[2] = createDosis(LocalTime.of(18, 0), aftenAntal);
+        doser[4] = createDosis(LocalTime.of(0, 0), natAntal);
+    }
+
+    private Dosis createDosis(LocalTime tid, double antal) {
+        Dosis dosis = new Dosis(tid, antal);
+        return dosis;
+    }
+
+    public Dosis[] getDoser() {
+        return doser.clone();
+    }
+
+    public void removeDosis(DagligFast dagligFast) { //Skal den fjerne alle doser eller skal man kunne fjerne 1?
+        if (doser[0] != null) {
+            Arrays.fill(doser, null);
+        }
+    }
+
+    @Override
+    public double samletDosis() {
+
+        return doegnDosis() * antalDage();
+    }
+
+    @Override
+    public double doegnDosis() {
+        double dognDosis = 0;
+
+        for (Dosis dosis : doser) {
+            dognDosis += dosis.getAntal();
+        }
+        return dognDosis;
+    }
+
+    @Override
+    public String getType() {
+
+        return "Daglig Fast";
+    }
 }
--- Medicinordination/src/ordination/PN.java
+++ Medicinordination/src/ordination/PN.java
@@ -1,10 +1,19 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PN {
+public class PN extends Ordination {
 
     private double antalEnheder;
+    private List<LocalDate> datoListe = new ArrayList<>();
+
+    public PN(LocalDate startDen, LocalDate slutDen, Patient patient, Laegemiddel laegemiddel, double antalEnheder) {
+        super(startDen, slutDen, patient, laegemiddel);
+        this.antalEnheder = antalEnheder;
+    }
 
     /**
      * Registrerer at der er givet en dosis paa dagen givesDen
@@ -14,19 +23,44 @@ public class PN {
      * @return
      */
     public boolean givDosis(LocalDate givesDen) {
-        // TODO
-        return false;   
+        boolean gyldigDosis = false;
+        if (givesDen.isAfter(super.getStartDen()) && givesDen.isBefore(super.getSlutDen())) {
+            gyldigDosis = true;
+            datoListe.add(givesDen);
+        }
+        return gyldigDosis;
     }
 
     public double doegnDosis() {
-        // TODO
-        return 0.0;
+        double dosis = (getAntalGangeGivet() * antalEnheder) / (ChronoUnit.DAYS.between(super.getStartDen(),super.getSlutDen()));
+        return dosis;
     }
 
+    @Override
+    public String getType() {
+        return "PN";
+    }
 
+    @Override
     public double samletDosis() {
-        // TODO
-        return 0.0;
+        double samletDosis = doegnDosis() * antalDage();
+        return samletDosis;
+    }
+
+    @Override
+    public int antalDage() {
+        LocalDate min = LocalDate.MAX;
+        LocalDate max = LocalDate.MIN;
+
+        for (LocalDate d : datoListe) {
+            if (d.isBefore(min)) {
+                min = d;
+            }
+            if (d.isAfter(max)) {
+                max = d;
+            }
+        }
+        return (int) ChronoUnit.DAYS.between(min, max) + 1;
     }
 
     /**
@@ -34,8 +68,8 @@ public class PN {
      * @return
      */
     public int getAntalGangeGivet() {
-        // TODO
-        return-1;
+        int antal = datoListe.size();
+        return antal;
     }
 
     public double getAntalEnheder() {

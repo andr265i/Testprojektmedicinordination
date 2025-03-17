5f5da6f7dc553818aa3f4f68f232b8e062f35902
vanessa <eaa24vtyt@students.eaaa.dk>
2025-03-17 11:29:46 +0100

    små rettelser

--- Medicinordination/Test/controller/ControllerTest.java
+++ Medicinordination/Test/controller/ControllerTest.java
@@ -1,14 +1,20 @@
 package controller;
 
+import ordination.Laegemiddel;
+import ordination.Patient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
+    private Patient patient;
+    private Laegemiddel laegemiddel;
 
     @BeforeEach
     void setUp() {
+        patient = new Patient("12346-7890","Bo Hansen",70);
+        laegemiddel = new Laegemiddel("Acetylsalicylsyre",4,5,6, "Styk");
     }
 
     @Test
